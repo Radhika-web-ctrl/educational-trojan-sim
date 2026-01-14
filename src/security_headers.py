16a61134568a43322727abe17b52952b981ee23c
Ahsan250229 <cihe250229@student.cihe.edu.au>
2026-01-14 14:00:58 +0500

    chore: add security headers middleware for application hardening

---
+++ src/security_headers.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from fastapi import Request
+from fastapi.responses import Response
+
+
+def add_security_headers(response: Response) -> Response:
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["Referrer-Policy"] = "no-referrer"
+    response.headers["Permissions-Policy"] = "geolocation=(), microphone=(), camera=()"
+    return response
